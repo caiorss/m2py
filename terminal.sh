9d0b74bcc9413fd55ef442578a10ff74b8142c80
CaioRS <caiorss.rodrigues@gmail.com>
2014-12-04 15:53:10 -0300

    added files

---
+++ terminal.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+# Auxiliary script to help open terminal consoles
+#
+$@
+sleep 1
+/bin/bash
---
+++ tests/test_numerical.py
@@ -0,0 +1,24 @@
+from math import *
+
+from m2py.numerical.numerical import bissection, RootFindErrror
+
+
+f = lambda x: exp(-x) -3*log(x)
+
+try:
+    root = bissection(f, 0.1, 1)
+    print root
+except RootFindErrror as err:
+    pass
+
+print "---------------------"
+
+
+
+print "---------------------"
+
+try:
+    root = bissection(f, 0.001, 1000, debug=True)
+    print root
+except Exception as err:
+    print err, err.msg
